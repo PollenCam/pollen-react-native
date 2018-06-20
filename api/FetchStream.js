137507cb060e6d3dd9f098e7c99ed5385d9ddd66
Kin Fong <bradley.kin.fong@gmail.com>
2018-06-20 16:20:03 -0500

    prototype get images via api

---
+++ api/FetchStream.js
@@ -0,0 +1,9 @@
+export const getStream = () => {
+  const url = 'http://localhost:3000/images';
+  const url2 = 'http://5b297ee284ce2c0014d4d17e.mockapi.io/events';
+  return fetch(url2)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error(error);
+    });
+};
--- containers/Stream.js
+++ containers/Stream.js
@@ -1,5 +1,88 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
+
+import {getStream} from '../api/FetchStream';
+
+
+class Stream extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      data: [],
+    };
+
+    this.renderItems = this.renderItems.bind(this);
+  }
+
+  componentDidMount() {
+    getStream()
+    .then((res) => {
+      if(res === undefined) {
+        this.setState({
+          error: 'Event not found'
+        });
+      } else {
+        this.setState({
+          data: res
+        })
+      }
+    })
+  }
+
+  renderItems() {
+    // console.log("dataLoaded = " + dataLoaded)
+    if (this.state.data && this.state.data.length > 0) {
+      // console.log("data = " + data)
+      return (
+        <FlatList
+          data={this.state.data}
+          renderItem={this._renderItem}
+          keyExtractor={this._keyExtractor}
+        />
+      );
+    } else {
+      return (
+        <Text>Error Data not loaded</Text>
+      )
+    }
+
+    // return this.state.data.map((dataItem, index) => {
+    //   return (
+    //     <View key = {index} style={styles.item}>
+    //       <Text>{dataItem.name}</Text>
+    //       <Image
+    //         style={{width: 500, height: 500}}
+    //         source={{uri: dataItem.imageUrl}}
+    //       />
+    //     </View>
+    //   )
+    // }
+    //
+    // );
+  }
+
+  _keyExtractor = (item, index) => index;
+
+  _renderItem = ({item}) => (
+        <View style={styles.item}>
+          <Text>{item.name}</Text>
+          <Image
+            style={{width: 500, height: 500}}
+            source={{uri: item.imageUrl}}
+          />
+        </View>
+  );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        { this.renderItems() }
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -8,16 +91,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#F4D35E',
   },
+  scrollView: {
+    flexDirection: 'row',
+  },
+  item: {
+    flex: 1,
+  },
 });
 
-class Stream extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Stream</Text>
-      </View>
-    );
-  }
-}
 
 export default Stream;
